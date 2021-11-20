c1e90e8bf6a6eb03312e6da0ba8ca1d4582118df
huybn5776 <aseeme555076@gmail.com>
2021-11-20 15:47:21 +0800

    feat: initial commit

---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<template>
+  <NConfigProvider class="page-layout" :theme="darkTheme">
+  </NConfigProvider>
+</template>
+
+<script setup lang="ts">
+// noinspection ES6UnusedImports
+import { darkTheme, NConfigProvider } from 'naive-ui';
+</script>
+
+<style lang="scss">
+@import 'App';
+</style>
