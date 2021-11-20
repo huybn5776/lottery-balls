5e3cbd1cf93baebfdc3ae4805ea20c99df73db30
huybn5776 <aseeme555076@gmail.com>
2021-11-20 16:18:06 +0800

    feat: octagon rotate button

--- src/components/Lottery.vue
+++ src/components/Lottery.vue
@@ -1,6 +1,11 @@
 <template>
   <div class="lottery-view">
     <div class="matter-container" ref="canvasContainer"></div>
+
+    <div class="rotate-action-container">
+      <NButton class="rotate-button" @mousedown="startRotateLeft">← Left</NButton>
+      <NButton class="rotate-button" @mousedown="startRotateRight">Right →</NButton>
+    </div>
   </div>
 </template>
 
@@ -8,7 +13,9 @@
 import { ref, onUnmounted } from 'vue';
 
 import { Body } from 'matter-js';
-import { Subject } from 'rxjs';
+// noinspection ES6UnusedImports
+import { NButton } from 'naive-ui';
+import { Subject, interval, takeUntil, fromEvent, startWith } from 'rxjs';
 
 import { autoPauseRender } from '@/auto-pause-render';
 import { createSense, createMouseConstraint } from '@modules/lottery/scenes';
@@ -16,7 +23,7 @@ import { useRenderer } from '@modules/lottery/use-renderer';
 
 const canvasContainer = ref<HTMLDivElement>();
 
-const octagonBound = ref<Body>();
+const octagonRef = ref<Body>();
 
 const destroy$ = new Subject<void>();
 
@@ -29,7 +36,7 @@ rendererReady$$.subscribe(({ renderer, engine, width, height }) => {
   });
 
   const { octagon } = createSense(engine.world, width, height);
-  octagonBound.value = octagon;
+  octagonRef.value = octagon;
 
   const mouse = createMouseConstraint(renderer.canvas, engine);
   renderer.addMouse(mouse);
@@ -39,6 +46,27 @@ onUnmounted(() => {
   destroy$.next();
   destroy$.complete();
 });
+
+function startRotate(event: MouseEvent, velocity: number): void {
+  interval(500)
+      .pipe(takeUntil(destroy$), takeUntil(fromEvent(event.target as HTMLElement, 'mouseup')), startWith(null))
+      .subscribe(() => rotateOctagonBound(velocity));
+}
+
+function startRotateLeft(event: MouseEvent): void {
+  startRotate(event, -0.03);
+}
+
+function startRotateRight(event: MouseEvent): void {
+  startRotate(event, 0.03);
+}
+
+function rotateOctagonBound(velocity: number): void {
+  if (!octagonRef.value) {
+    return;
+  }
+  Body.setAngularVelocity(octagonRef.value, velocity);
+}
 </script>
 
 <style lang="scss" scoped>
